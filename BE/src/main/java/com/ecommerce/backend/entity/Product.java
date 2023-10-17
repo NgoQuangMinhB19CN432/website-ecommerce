57bd70fae92f2c02c86cf34689328515d06b3ff3
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-10-17 08:04:56 +0700

    create base entity

---
+++ BE/src/main/java/com/ecommerce/backend/entity/Product.java
@@ -0,0 +1,85 @@
+package com.ecommerce.backend.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productId;
+    private String productName;
+    @Column(length = 2000)
+    private String productDescription;
+    private Double productDiscountedPrice;
+    private Double productActualPrice;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "product_images",
+            joinColumns = {
+                    @JoinColumn(name = "product_id")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "image_id")
+            }
+    )
+//    private Set<ImageModel> productImages;
+//
+//    public Set<ImageModel> getProductImages() {
+//        return productImages;
+//    }
+//
+//    public void setProductImages(Set<ImageModel> productImages) {
+//        this.productImages = productImages;
+//    }
+
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Double getProductDiscountedPrice() {
+        return productDiscountedPrice;
+    }
+
+    public void setProductDiscountedPrice(Double productDiscountedPrice) {
+        this.productDiscountedPrice = productDiscountedPrice;
+    }
+
+    public Double getProductActualPrice() {
+        return productActualPrice;
+    }
+
+    public void setProductActualPrice(Double productActualPrice) {
+        this.productActualPrice = productActualPrice;
+    }
+}
