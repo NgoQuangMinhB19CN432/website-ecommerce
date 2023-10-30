719f93e84bcf4a28d7b806dce4c2cf6e33eeaaa7
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-10-31 05:17:52 +0700

    create service

---
+++ BE/src/main/java/com/ecommerce/backend/service/CartService.java
@@ -0,0 +1,60 @@
+package com.ecommerce.backend.service;
+
+
+import com.ecommerce.backend.configuration.JwtRequestFilter;
+import com.ecommerce.backend.dao.*;
+import com.ecommerce.backend.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class CartService {
+
+    @Autowired
+    private CartDao cartDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    public void deleteCartItem(Integer cartId) {
+        cartDao.deleteById(cartId);
+    }
+
+    public Cart addToCart(Integer productId) {
+        Product product = productDao.findById(productId).get();
+
+        String username = JwtRequestFilter.CURRENT_USER;
+
+        User user = null;
+        if(username != null) {
+            user = userDao.findById(username).get();
+        }
+
+        List<Cart> cartList = cartDao.findByUser(user);
+        List<Cart> filteredList = cartList.stream().filter(x -> Objects.equals(x.getProduct().getProductId(), productId)).collect(Collectors.toList());
+
+        if(!filteredList.isEmpty()) {
+            return null;
+        }
+
+        if(product != null && user != null) {
+            Cart cart = new Cart(product, user);
+            return cartDao.save(cart);
+        }
+
+        return null;
+    }
+
+    public List<Cart> getCartDetails() {
+        String username = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(username).get();
+        return cartDao.findByUser(user);
+    }
+}
---
+++ BE/src/main/java/com/ecommerce/backend/service/OrderDetailService.java
@@ -0,0 +1,140 @@
+package com.ecommerce.backend.service;
+
+
+import com.ecommerce.backend.configuration.JwtRequestFilter;
+import com.ecommerce.backend.dao.CartDao;
+import com.ecommerce.backend.dao.OrderDetailDao;
+import com.ecommerce.backend.dao.ProductDao;
+import com.ecommerce.backend.dao.UserDao;
+import com.ecommerce.backend.entity.Cart;
+import com.ecommerce.backend.entity.OrderDetail;
+import com.ecommerce.backend.entity.OrderInput;
+import com.ecommerce.backend.entity.OrderProductQuantity;
+import com.ecommerce.backend.entity.Product;
+import com.ecommerce.backend.entity.TransactionDetails;
+import com.ecommerce.backend.entity.User;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONException;
+
+@Service
+public class OrderDetailService {
+
+    private static final String ORDER_PLACED = "Placed";
+
+    private static final String KEY = "rzp_test_AXBzvN2fkD4ESK";
+    private static final String KEY_SECRET = "bsZmiVD7p1GMo6hAWiy4SHSH";
+    private static final String CURRENCY = "INR";
+
+    @Autowired
+    private OrderDetailDao orderDetailDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private CartDao cartDao;
+
+    public List<OrderDetail> getAllOrderDetails(String status) {
+        List<OrderDetail> orderDetails = new ArrayList<>();
+
+        if(status.equals("All")) {
+            orderDetailDao.findAll().forEach(
+                    x -> orderDetails.add(x)
+            );
+        } else {
+            orderDetailDao.findByOrderStatus(status).forEach(
+                    x -> orderDetails.add(x)
+            );
+        }
+
+
+         return orderDetails;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        String currentUser = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(currentUser).get();
+
+        return orderDetailDao.findByUser(user);
+    }
+
+    public void placeOrder(OrderInput orderInput, boolean isSingleProductCheckout) {
+        List<OrderProductQuantity> productQuantityList = orderInput.getOrderProductQuantityList();
+
+        for (OrderProductQuantity o: productQuantityList) {
+            Product product = productDao.findById(o.getProductId()).get();
+
+            String currentUser = JwtRequestFilter.CURRENT_USER;
+            User user = userDao.findById(currentUser).get();
+
+            OrderDetail orderDetail = new OrderDetail(
+                  orderInput.getFullName(),
+                  orderInput.getFullAddress(),
+                  orderInput.getContactNumber(),
+                  orderInput.getAlternateContactNumber(),
+                    ORDER_PLACED,
+                    product.getProductDiscountedPrice() * o.getQuantity(),
+                    product,
+                    user,
+                    orderInput.getTransactionId()
+            );
+
+            // empty the cart.
+            if(!isSingleProductCheckout) {
+                List<Cart> carts = cartDao.findByUser(user);
+                carts.stream().forEach(x -> cartDao.deleteById(x.getCartId()));
+            }
+
+            orderDetailDao.save(orderDetail);
+        }
+    }
+
+    public void markOrderAsDelivered(Integer orderId) {
+        OrderDetail orderDetail = orderDetailDao.findById(orderId).get();
+
+        if(orderDetail != null) {
+            orderDetail.setOrderStatus("Delivered");
+            orderDetailDao.save(orderDetail);
+        }
+
+    }
+
+    public TransactionDetails createTransaction(Double amount) throws RazorpayException {
+        try {
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("amount", (amount * 100) );
+            jsonObject.put("currency", CURRENCY);
+
+            RazorpayClient razorpayClient = new RazorpayClient(KEY, KEY_SECRET);
+
+            Order order = razorpayClient.orders.create(jsonObject);
+
+            TransactionDetails transactionDetails =  prepareTransactionDetails(order);
+            return transactionDetails;
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private TransactionDetails prepareTransactionDetails(Order order) {
+        String orderId = order.get("id");
+        String currency = order.get("currency");
+        Integer amount = order.get("amount");
+
+        TransactionDetails transactionDetails = new TransactionDetails(orderId, currency, amount, KEY);
+        return transactionDetails;
+    }
+}
